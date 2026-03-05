341452a1490e92d119467ea93991f999a528eea3
ibraX <ibrahimnaik91@gmail.com>
2026-03-05 23:57:15 +0300

    practiced both conceptual and practical deep on the javascrip oop

---
+++ javascript/9. prototype-inheritance/README.md
@@ -0,0 +1,11 @@
+# Prototype & Inheritance Practice
+
+Experiments covering:
+
+- Object prototypes
+- Constructor functions
+- new keyword behavior
+- Prototype chain lookup
+- ES6 class syntax vs manual prototypes
+
+Goal: Understand how inheritance works under the hood.
--- javascript/9. prototype-inheritance/test.js
+++ javascript/9. prototype-inheritance/test.js
@@ -1,4 +1,55 @@
-const obj = { name: "Prototype" };
+// const obj = { name: "Prototype" };
 
-console.log(obj.__proto__ === Object.prototype); // true
-console.log(Object.prototype.__proto__); // null
+// console.log(obj.__proto__ === Object.prototype); // true
+// console.log(Object.prototype.__proto__); // null
+
+
+// console.log(Object.create);
+
+
+// const animal = {
+//   speak() {
+//     console.log("Animal speaks");
+//   }
+// };
+
+// const dog = {}
+
+// dog.speak = function () {
+//   console.log("Dog barks");
+// };
+
+// dog.speak();
+
+
+// const a = {};
+// console.log(Object.getPrototypeOf(a));
+// console.log(a.toString());
+
+
+
+// const b = Object.create(null);
+// console.log(Object.getPrototypeOf(b));
+// console.log(b.toString());      // error
+
+
+
+// const obj = { name: "Prototype" };
+
+// console.log(obj.__proto__ === Object.prototype); // true
+// console.log(Object.prototype.__proto__); // null
+
+
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayHello = function () {
+  console.log("Hello, my name is", this.name);
+};
+
+const p1 = new Person("Alice");
+
+p1.sayHello();
+
+
